b28488a10dcaa16b10d3d3e4598b66079c80674d
chkg2a <xshalltearvamps@gmail.com>
2024-12-03 21:27:23 +0530

    feat(algo): add multiple sorting algo

--- includes/fn_algorithms.h
+++ includes/fn_algorithms.h
@@ -6,7 +6,10 @@ namespace chk{
 int binary_search(const int arr[], const int &size, const int &toSearch);
 int linear_search(const int arr[], const int &size, const int &toSearch);
 
-void selectionSort(int arr[], const int & size);
+void bubble_sort(int arr[], const int & size);
+void selection_sort(int arr[], const int & size);
+void insertion_sort(int arr[], const int & size);
+void merge_sort(int arr[], const int & size, const int & left, const int & right);
 }
 
 #endif // !FN_ALGORITHMS_H
